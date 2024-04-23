26b2f05dcfe4b1929ea1e28326d2dd0ed864d5ad
brendojonch <77908519+brendojonch@users.noreply.github.com>
2024-04-23 15:39:17 -0300

    Create script-betuxo.js

---
+++ script-betuxo.js
@@ -0,0 +1,6 @@
+
+  document.title = "Fortune Dragon: Multiplicadores Mágicos | Aposta Mínima de R$0,40 na UPBET";
+  var metaDescription = document.createElement('meta');
+  metaDescription.name = "description";
+  metaDescription.content = "Junte-se à aventura épica com Fortune Dragon na UPBET. Com uma aposta mínima de apenas R$0,40, mergulhe em um mundo onde dragões simbolizam sorte e prosperidade, multiplicando suas apostas de forma espetacular. Desfrute de um RTP impressionante de 96,74% em uma configuração de bobinas 3×3, repleta de ação com recursos como a Funcionalidade do Dragão da Fortuna, Modo “Wild” e Bobina Multiplicadora. Experimente uma jogabilidade dinâmica com 5 possibilidades de vitória e uma volatilidade média. Busque a fortuna máxima de 2.500x e torne-se o próximo grande vencedor!";
+  document.getElementsByTagName('head')[0].appendChild(metaDescription);
